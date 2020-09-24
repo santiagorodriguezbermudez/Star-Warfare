960792f35786a6e20c1641db3fa1c370415f77d4
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-09-24 15:04:31 -0500

    Load new assets and enemy

--- src/js/scenes/loading_scene.js
+++ src/js/scenes/loading_scene.js
@@ -65,19 +65,33 @@ class LoadingScene extends Phaser.Scene {
     this.load.image('player_live', 'src/assets/images/player_live.png');
     [1, 2, 3, 4, 5, 6, 7, 8].forEach((el) => this.load.image(`player${el}`, `src/assets/images/animation_player/${el}.png`));
 
+    // Enemies assets
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].forEach((el) => this.load.image(`garbage${el}`, `src/assets/images/animation_garbage/garbage_${el}.png`));
+
+    // Game assets
     this.load.image('start_logo', 'src/assets/images/start_logo.png');
     this.load.image('start_btn', 'src/assets/images/start_btn.png');
     this.load.image('start_btn_hover', 'src/assets/images/start_btn_hover.png');
     this.load.image('leader_btn', 'src/assets/images/leader_btn.png');
     this.load.image('leader_btn_hover', 'src/assets/images/leader_btn_hover.png');
 
+    // Other animations
+    this.load.spritesheet('animationExplosion', 'src/assets/images/explosion.png', {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
+
     // Download all of the audios
     // Main Menu
     this.load.audio('menu_audio', ['src/assets/audio/menu_audio.mp3']);
     this.load.audio('stage1_audio', ['src/assets/audio/stage1_audio.mp3']);
 
+    // Dowload audios
     // Player Audios
     this.load.audio('player_laser_audio', ['src/assets/audio/player_laser.ogg']);
+
+    // Explosion audios
+    [1, 2, 3].forEach((el) => this.load.audio(`explosion${el}`, `src/assets/audio/explosion${el}.mp3`));
   }
 
   ready() {
