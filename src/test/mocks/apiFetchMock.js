718e470761724933cdc49028d69b07c8695e20cd
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-09-27 14:55:31 -0500

    Added mocks for unit testing

---
+++ src/test/mocks/apiFetchMock.js
@@ -0,0 +1,27 @@
+const saveScore = async (name, score) => {
+  const api = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/h0z4c9vjaT6T11L51p9A/scores/';
+
+  const jsonObj = {
+    name,
+    score,
+  };
+  try {
+    const resp = await fetch(api, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(jsonObj),
+    });
+    if (resp.ok) {
+      await resp.json();
+      return 'Everything is working just fine!';
+    }
+    throw new Error('Request Failed');
+  } catch (error) {
+    return 'Error found';
+  }
+};
+
+export default saveScore;
---
+++ src/test/mocks/scoreMock.js
@@ -0,0 +1,3 @@
+const getScores = () => Promise.resolve({ user: 'testUserName', score: 1000 });
+
+export default getScores;
