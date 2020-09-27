4c15f9d9492d274d1cd62555cd56db70b40fcb8c
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-09-27 17:11:44 -0500

    Fized testing configuration

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -1,5 +1,4 @@
 import Phaser from 'phaser';
-import UIPlugin from 'phaser3-rex-plugins/templates/ui/ui-plugin';
 
 export default {
   type: Phaser.AUTO,
@@ -17,14 +16,6 @@ export default {
       gravity: { x: 0, y: 0 },
       debug: false,
     },
-  },
-  plugins: {
-    scene: [{
-      key: 'rexUI',
-      plugin: UIPlugin,
-      mapping: 'rexUI',
-    },
-    ],
   },
   pixelArt: true,
   roundPixels: true,
